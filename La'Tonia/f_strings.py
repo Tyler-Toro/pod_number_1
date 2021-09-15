4beaa042305eef235176001973099713eda366a2
La'Tonia Mertica <latoniamertica@gmail.com>
2021-09-15 16:08:22 -0400

    edited readme file for bold and italic to work and added two files - pemdas and f strings

---
+++ La'Tonia/f_strings.py
@@ -0,0 +1,4 @@
+profit = 44000
+my_f_string = f'The profit this month for La\'Tonia Mertica\'s exceptional code/web dev work is ${profit}'
+print(my_f_string)
+print(f'I earn $444 in a day and my expenses are $44 a day. My profit is ${444 - 44}')
