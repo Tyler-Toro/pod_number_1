5430463a51e45cba8eeb203d6c59e42cd449566b
Jason Doze <jdoze@protonmail.com>
2021-09-15 19:28:39 -0500

    adding my first challenge to pod #1 repo

---
+++ Jason/first_python_challenge.py
@@ -0,0 +1,35 @@
+print('1: Describe what is happening below by adding comments before each line\n')
+# variable with books in it
+box_1 = 'books' 
+# variable assigned to a value of 'clothes'
+box_2 = 'clothes'
+# box_3 contains a string plants
+box_3 = 'plants'
+# box_4 equals kitchen stuff
+box_4 = 'kitchen stuff'
+
+print('2: Print the variables box_1, box_2, box_3, box_4\n')
+print(box_1)
+print(box_2)
+print(box_3)
+print(box_4)
+
+print('3.1: Declare a variable with the name "address", assign it any street name you like\n')
+address = 'Amsterdam_Avenue'
+print('3.2: Print the address variable\n')
+print(address)
+
+print('4: Reassign variables box_2 and box_4 with some other text and print box_1, box_2, box_3, box_4 again\n')
+box_2 = 'monkeys'
+box_4 = 'tickets'
+print(box_1)
+print(box_2)
+print(box_3)
+print(box_4)
+
+print('5.1: The line of code below is commented out because it produces many SyntaxErrors. Fix the problem and turn the comment back into regular Python code\n')
+completion_message = 'Completed the first Python challenge!'
+
+print('5.2: Turn the comment below back into regular Python code\n')
+print(completion_message)
+
