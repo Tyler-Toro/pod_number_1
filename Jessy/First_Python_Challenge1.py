95320e5e0818b1bda912ede81d27ac4d24ceb365
Vamonosraza <martinezjessy393@gmail.com>
2021-09-19 20:59:32 -0700

    Here are my boxes of items I no longer want

---
+++ Jessy/First_Python_Challenge1.py
@@ -0,0 +1,35 @@
+print('1: Describe what is happening below by adding comments before each line\n')
+# assigning variable box_1=books
+box_1 = 'books'
+# variable assigned to a value of clothes
+box_2 = 'clothes'
+# variable contains a string called 'plants'
+box_3 = 'plants'
+# variable box_4 assigned 'kitchen stuff'
+box_4 = 'kitchen stuff'
+
+print('2: Print the variables box_1, box_2, box_3, box_4\n')
+print(box_1)
+print(box_2)
+print(box_3)
+print(box_4)
+
+print('3.1: Declare a variable with the name "address", assign it any street name you like\n')
+address = 'Story Ave'
+
+print('3.2: Print the address variable')
+print(address)
+
+print('4: Reassign variables box_2 and box_4 with some other text and print box_1, box_2, box_3, box_4 again\n')
+print(box_1)
+box_2 = 'Oranges'
+print(box_2)
+print(box_3)
+box_4 = 'Apples'
+print(box_4)
+
+print('5.1: The line of code below is commented out because it produces many SyntaxErrors. Fix the problem and turn the comment back into regular Python code\n')
+completionmessage = 'Completed the first Python challenge!'
+
+print('5.2: Turn the comment below back into regular Python code\n')
+print(completionmessage)
