f5b322912bf24e9b75676cb1c1db2f1735a4ba3a
Brandon <Grant.b2134@gmail.com>
2021-09-22 23:59:36 -0400

    adding snippets, stocks and box office challenges

--- challenges/06_bootcamp_logic/snippets_challenge.py
+++ challenges/06_bootcamp_logic/snippets_challenge.py
@@ -4,11 +4,12 @@ print("Challenge 3.1: Debug code snippets")
 print()
 
 print("Code Snippet 1:")
-
+                 
 u = 5
 v = 2
 
-if u * v = 10:
+if u * v == 10:
+    # added another '=' so we are not assigning and making the value equal
     print(f"The product of u ({u}) and v ({v}) is 10")
 else:
     print(f"The product of u ({u}) and v ({v}) is not 10")
@@ -24,10 +25,12 @@ z = 30
 if z < x:
     print("z is less than x")
 
-elif z > x and z < y
+elif z > x and z < y:
+    # added a colon
     print("z is between x and y")
 
-else
+else:
+    # added
     print("z is greater than y")
 
 
@@ -40,7 +43,8 @@ print("Code Snippet 3:")
 
 a = 1
 b = 1
-c = (a > b)
+c = (a >= b)
+# added an equal sign
 
 print(f"The value of c ({c}) is True since a ({a}) is greater than b ({b}).")
 assert(c == True) #Do not change this line
@@ -51,10 +55,12 @@ print("Code Snippet 4:")
 
 #modify exactly one boolean operator in the assignment of d, so that d evaluates to False
 
-d = (5 < 7) or not (8 < 20)
+d = (5 < 7) and not  (8 < 20)
 # TO DO: Explain how d is set to False in a print statement
+# its set to False w/ the erasing of 'or' and replacing it w/  'and'
+# added a print statement
 assert(d == False) #Do not change this line
-
+print(d)
 print()
 
 
@@ -65,8 +71,8 @@ print("Code Snippet 5:")
 
 m = "GOAT"
 n = "goat"
-
-o = (m == n)
+# changed the == to != to reflect the true statement.
+o = (m != n)
 
 print (f"The value of o ({o}) is True since Python is case-sensitive.")
 assert(o == True) #Do not change this line
--- challenges/06_bootcamp_logic/stocks_challenge.py
+++ challenges/06_bootcamp_logic/stocks_challenge.py
@@ -14,27 +14,56 @@ print("Challenge 3.2.1: Taking user input")
 # TODO: Write code to ask the client his name and save it to a variable.
 # TODO: Write code to ask the client his savings and save it to another variable.
 # TODO: Write code to ask the client the stock he is interested in and save it to another variable, as shown below.
+
+# assigned variables name, savings, stock and num of stock.
+name = input("What is your name?")
+savings = int(input("How much savings do you have?"))
 stock = input("Which stock are you interested in? Type 'amzn' for Amazon, 'aapl' for Apple, 'fb' for Facebook, 'goog' for Google and 'msft' for Microsoft.")
+num_of_stocks = 0
+
 print()
 
-print("Challenge 3.2.2: Perform user-specific calculations")
-# TODO: You have all 3 user inputs stores in variables. Based on that, write conditional (if-elif-else) statements to find out the number of stocks of the company that can be purchased with the savings amount.
-
+# print("Challenge 3.2.2: Perform user-specific calculations")
+# # TODO: You have all 3 user inputs stores in variables. Based on that, write conditional (if-elif-else) statements to find out the number of stocks of the company that can be purchased with the savings amount.
 '''
 Your code should look like this:
-
 if stock == "amzn":
     ...
 elif ...
 else ...
 '''
+# made if/elif/else statemenets to reflect desired outcome.
 
-print()
+if stock == "amzn":
+    price = amazon
+    num_of_stocks = savings/price
+elif stock == "aapl":
+    price = apple
+    num_of_stocks = savings/price
+elif stock == "fb":
+    price = fb
+    num_of_stocks = savings/price
+elif stock == "goog":
+    price = google
+    num_of_stocks = savings/price
+elif stock == "msft":
+    price = msft
+    num_of_stocks = savings/price
+else:
+    price = "NA"
 
-print("Challenge 3.2.3: Output for the user the result")
-# TODO: COnce you have calculated the number of stocks that can be purchased, print the result for the client. Result should be in a format like this:
+# print()
 
-# Alex has $5000 in savings and he can buy 50 shares of Apple at the current price of $100.
+# print("Challenge 3.2.3: Output for the user the result")
+# # TODO: COnce you have calculated the number of stocks that can be purchased, print the result for the client. Result should be in a format like this:
+# # Alex has $5000 in savings and he can buy 50 shares of Apple at the current price of $100.
 
-print()
+# made if/elif/else statemenets to reflect desired outcome.
+if price == "NA":
+    print("Incorrect stock name entered.")
+else:
+    print(f"{name} has ${savings} in savings and he can buy {num_of_stocks} shares of {stock} at the current price of ${price}.")
+
+
+# print()
 
