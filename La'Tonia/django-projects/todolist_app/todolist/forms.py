ca1c6c7db0290eacf23b37225f12f1524fce7f46
La'Tonia Mertica <latoniamertica@gmail.com>
2021-11-02 23:56:15 -0400

    as close to best case design scenario to date

--- La'Tonia/django-projects/todolist_app/todolist/forms.py
+++ La'Tonia/django-projects/todolist_app/todolist/forms.py
@@ -1,9 +1,9 @@
 from django import forms
 
 class TodoForm(forms.Form):
-    task = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'enter task', 'style': 'width:90%'}), 
+    task = forms.CharField(widget=forms.TextInput(attrs={'placeholder': '   enter task', 'style': 'width:90%'}), 
     label = '', max_length = 255)
 
 class NoteForm(forms.Form):
-    notes = forms.CharField(widget=forms.TextInput(attrs={'style': 'width:90%', 'style': 'padding: 20%'}), max_length= 444, label = '')
+    notes = forms.CharField(widget=forms.TextInput(attrs={'style': 'width: 99.25%'}), max_length= 444, label = '')
 
